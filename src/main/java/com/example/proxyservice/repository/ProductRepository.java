a7105b8d55017c08e7d78419c22a9c191c373b3d
Rusheendra reddy Yellaturi <rusheendrared.yella@hcl.com>
2023-10-30 22:19:18 +0530

    Product repo tests

--- src/main/java/com/example/proxyservice/repository/ProductRepository.java
+++ src/main/java/com/example/proxyservice/repository/ProductRepository.java
@@ -4,6 +4,16 @@ import com.example.proxyservice.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    //List<Product> getProductsOrderByPriceDesc();
+    List<Product> findAllProductsByOrderByPriceDesc();
+
+    List<Product> findAllByPriceBetweenOrderByPriceDesc(int start, int end);
+
+    List<Product> deleteAllByPriceBetweenOrderByPriceDesc(int start, int end);
+
+    List<Product> findAllByOrderByIdDesc();
 }
--- src/test/java/com/example/proxyservice/repository/ProductRepoTest.java
+++ src/test/java/com/example/proxyservice/repository/ProductRepoTest.java
@@ -6,7 +6,9 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
@@ -20,6 +22,7 @@ public class ProductRepoTest {
 
     @Test
     @Transactional
+    //@Rollback(value = false)
     void saveProductAndCategories(){
         Categories categories = new Categories();
         categories.setName("Electronics");
@@ -38,6 +41,7 @@ public class ProductRepoTest {
 
     @Test
     @Transactional
+    //@Rollback(value = false)
     void saveProductsAndCategory() {
         Categories categories = new Categories();
         categories.setName("Electronics");
@@ -47,17 +51,19 @@ public class ProductRepoTest {
         Product product = new Product();
         product.setTitle("Laptop");
         product.setDescription("Laptop");
+        product.setPrice(25000d);
         product.setCategory(categories);
         productRepository.save(product);
 
-        Categories categories1 = categoryRepository.findById(1L).get();
-        List<Product> productList = categories1.getProductList();
+//        Categories categories1 = categoryRepository.findById(1L).get();
+//        List<Product> productList = categories1.getProductList();
         System.out.println("Debug");
 
     }
 
     @Test
     @Transactional
+    //@Rollback(value = false)
     void saveProductAndCategories2(){
         Categories categories = new Categories();
         categories.setName("Fashion");
@@ -68,11 +74,78 @@ public class ProductRepoTest {
         product.setTitle("Strapless Dress");
         product.setCategory(categories);
         product.setDescription("Newest Exclusive Zara product");
-        product.setPrice(1000);
+        product.setPrice(1500);
         productRepository.save(product);
 
         System.out.println("Debug");
     }
+
+    @Test
+    @Transactional
+    void increaseAllIsByOne(){
+        List<Product> productsOld = productRepository.findAll();
+        List<Product> updatedProducts = new ArrayList<>();
+        for(Product product : productsOld){
+            product.setPrice(product.getPrice()+1);
+           Product updatedProduct  = productRepository.save(product);
+           updatedProducts.add(updatedProduct);
+        }
+
+        System.out.println("Debug");
+    }
+
+    @Test
+    @Transactional
+    void getProductsInSortedOrder(){
+        List<Product> products = productRepository.findAllProductsByOrderByPriceDesc();
+        System.out.println("Debug");
+
+        for(int i=1;i<products.size();i++){
+            assert(products.get(i-1).getPrice()>=products.get(i).getPrice());
+        }
+    }
+
+    @Test
+    @Transactional
+    void getProductsInPriceRange(){
+        List<Product> products = productRepository.findAllByPriceBetweenOrderByPriceDesc(1000,2000);
+        System.out.println("Debug");
+
+        for(int i=1;i<products.size();i++){
+            assert(products.get(i-1).getPrice()>=products.get(i).getPrice());
+            assert(products.get(i).getPrice()>=1000);
+            assert(products.get(i).getPrice()<=2000);
+        }
+    }
+
+    @Test
+    @Transactional
+    void deleteProductsInPriceRange(){
+        List<Product> products = productRepository.deleteAllByPriceBetweenOrderByPriceDesc(1000,2000);
+        System.out.println("Debug");
+
+        for(int i=1;i<products.size();i++){
+            System.out.println(products.get(i));
+            assert(products.get(i-1).getPrice()>=products.get(i).getPrice());
+            assert(products.get(i).getPrice()>=1000);
+            assert(products.get(i).getPrice()<=2000);
+        }
+    }
+
+    @Test
+    @Transactional
+    void getProductsInDescendingOrder(){
+        List<Product> products = productRepository.findAllByOrderByIdDesc();
+
+        System.out.println(products.get(0));
+        for(int i=1;i<products.size();i++){
+            System.out.println(products.get(i));
+            assert(products.get(i-1).getId()>=products.get(i).getId());
+        }
+    }
+
+
+
 
 
 }
